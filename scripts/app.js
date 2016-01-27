1598856dc17488483229d9e18780ebd59fc66bd3
Julie Vojczekhovskiy <j.vojczekhovskiy@mondriaanict.nl>
2016-01-27 17:01:31 +0100

    sdk toegevoegd

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,1 @@
-angular.module("boekingApp", []);
+angular.module("boekingApp", ['ngResource','lbServices']);
--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('boekingApp')
-.controller('mainCtrl', function($scope, dataService) {
+.controller('mainCtrl', function($scope, dataService, Booking) {
   $scope.boekingen = [];
   $scope.maanden = ["Januari","Februari","Maart","April","Mei","Juni","Juli","Augustus","September","Oktober","November","December"];
   $scope.dagen =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
@@ -10,7 +10,10 @@ angular.module('boekingApp')
   $scope.addBoeking = function(n,i,d,da,m,t) {
     var boeking = {name: n, info: i, doel: d, tijd: da+" "+m+" "+t};
     $scope.boekingen.push(boeking);
+
   };
+
+//  $scope.boekingen[0] = Booking.findById({ id: '56a8e4e8106f1ac822e268e3' });
 
   $scope.resetInput = function(){
     document.getElementsByClassName('form-horizontal')[0].reset();
