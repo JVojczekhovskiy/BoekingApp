b88d7718e296dfc5673ebd2f17148e283372848c
Julie Vojczekhovskiy <j.vojczekhovskiy@mondriaanict.nl>
2016-01-28 10:14:13 +0100

    Updated

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -1,8 +1,10 @@
 'use strict';
 
 angular.module('boekingApp')
-.controller('mainCtrl', function($scope, dataService, Booking) {
-  $scope.boekingen = [];
+.controller('mainCtrl', function($scope,Booking) {
+  $scope.boekingen = Booking.find({
+    filter: { limit: 10 }
+  });
   $scope.maanden = ["Januari","Februari","Maart","April","Mei","Juni","Juli","Augustus","September","Oktober","November","December"];
   $scope.dagen =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
   $scope.tijden = ["09:00","09:30","10:00","10:30","11:00","11:30","12:00","12:30","13:00","13:30","14:00","14:30","15:00","15:30","16:00","16:30"];
@@ -19,8 +21,7 @@ angular.module('boekingApp')
     document.getElementsByClassName('form-horizontal')[0].reset();
   }
 
-  $scope.deleteBoeking = function(boeking, $index) {
-    dataService.deleteBoeking(boeking);
+  $scope.deleteBoeking = function($index) {
     $scope.boekingen.splice($index, 1);
   };
 })
