89fcdcfe8f689ab982807cc3d92b01b4bbc3f70e
Julie Vojczekhovskiy <j.vojczekhovskiy@mondriaanict.nl>
2016-01-29 16:28:57 +0100

    Replaced Booking.find() with Booking.query()

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -2,7 +2,7 @@
 
 angular.module('boekingApp')
 .controller('mainCtrl', function($scope,Booking) {
-  $scope.boekingen = Booking.find();
+  $scope.boekingen = Booking.query();
   $scope.maanden = ["January","February","March","April","May","June","July","August","September","October","November","December"];
   $scope.dagen =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
   $scope.tijden = ["09:00","09:30","10:00","10:30","11:00","11:30","12:00","12:30","13:00","13:30","14:00","14:30","15:00","15:30","16:00","16:30"];
